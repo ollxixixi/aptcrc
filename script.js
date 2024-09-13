bc69586547635738ecc9889bbdebed2050c911c4
oll <806232525@qq.com>
2024-09-13 11:23:26 +0800

    2nd

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function showModule(module) {
     // 显示选中的模块
     document.getElementById(module).style.display = 'block';
 }
+
+function showField(field) {
+    // 隐藏所有模块 windField  windSpeed
+    document.getElementById('windField').style.display = 'none';
+    document.getElementById('windSpeed').style.display = 'none';
+
+    // 显示选中的模块
+    document.getElementById(field).style.display = 'block';
+}
